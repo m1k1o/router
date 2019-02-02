106b05c751024b5a69b8540807ba18b1a223b8f6
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-02 23:38:27 +0100

    ARP Table

---
+++ Router/ARPEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Router
+{
+    class ARPEntry
+    {
+        public IPAddress IPAddress;
+        public PhysicalAddress PhysicalAddress;
+        public long TTL;
+
+        public ARPEntry(IPAddress IPAddress, PhysicalAddress PhysicalAddress, long TTL)
+        {
+            this.IPAddress = IPAddress;
+            this.PhysicalAddress = PhysicalAddress;
+            this.TTL = TTL;
+        }
+    }
+}
---
+++ Router/ARPTable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Router
+{
+    class ARPTable
+    {
+        internal static ARPTable Instance { get; } = new ARPTable();
+
+        public int TTL = 128;
+
+        private List<ARPEntry> Entries = new List<ARPEntry>();
+
+        public void Push(IPAddress IPAddress, PhysicalAddress PhysicalAddress)
+        {
+            long timeStamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+
+            foreach (var Entry in Entries)
+            {
+                if (Equals(Entry.IPAddress, IPAddress))
+                {
+                    Entry.PhysicalAddress = PhysicalAddress;
+                    Entry.TTL = timeStamp + TTL;
+                    return;
+                }
+            }
+
+            Entries.Add(new ARPEntry(IPAddress, PhysicalAddress, timeStamp + TTL));
+        }
+
+        public PhysicalAddress Find(IPAddress IPAddress)
+        {
+            long timeStamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+
+            foreach (var Entry in Entries)
+            {
+                if (Equals(Entry.IPAddress, IPAddress))
+                {
+                    if (Entry.TTL < timeStamp)
+                    {
+                        return null;
+                    }
+
+                    return Entry.PhysicalAddress;
+                }
+            }
+
+            return null;
+        }
+
+        public void Flush()
+        {
+            Entries = new List<ARPEntry>();
+        }
+
+        public List<ARPEntry> GetEntries()
+        {
+            return Entries;
+        }
+    }
+}
