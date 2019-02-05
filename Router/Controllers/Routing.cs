536420a3d65b6dfbce2e237754547e9c94640dcc
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-05 03:06:27 +0100

    Routing Table controller.

---
+++ Router/Controllers/Routing.cs
@@ -0,0 +1,37 @@
+﻿using Router.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router.Controllers
+{
+    class Routing
+    {
+        static private readonly RoutingTable RoutingTable = RoutingTable.Instance;
+
+        public JSON Table(string Data = null)
+        {
+            var arr = new JSONArray();
+            var obj = new JSONObject();
+
+            var Rows = RoutingTable.GetEntries();
+            foreach (var Row in Rows)
+            {
+                obj.Empty();
+
+                obj.Push("ip", Row.IPNetwork.NetworkAddress);
+                obj.Push("mask", Row.IPNetwork.SubnetMask);
+                obj.Push("network", Row.IPNetwork);
+                obj.Push("next_hop", Row.NextHopIP);
+                obj.Push("interface", Row.Interface);
+                obj.Push("type", Row.ADistance);
+
+                arr.Push(obj);
+            }
+
+            return arr;
+        }
+    }
+}
