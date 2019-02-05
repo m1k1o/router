2f7c5360082f20234ccf2867dfd9f321354b993b
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-05 02:31:53 +0100

    Routing Table

---
+++ Router/RoutingEntry.cs
@@ -0,0 +1,38 @@
+﻿using Router.Helpers;
+using System.Net;
+
+namespace Router
+{
+    class RoutingEntry
+    {
+        public IPNetwork IPNetwork;
+        public IPAddress NextHopIP;
+        public Interface Interface;
+        public ADistance ADistance;
+
+        public RoutingEntry(IPNetwork IPNetwork, IPAddress NextHopIP, Interface Interface, ADistance ADistance)
+        {
+            this.IPNetwork = IPNetwork;
+            this.NextHopIP = NextHopIP;
+            this.Interface = Interface;
+            this.ADistance = ADistance;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = IPNetwork.GetHashCode();
+                hashCode = (hashCode * 397) ^ ADistance.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+
+    public enum ADistance : byte
+    {
+        DirectlyConnected = 0,
+        Static = 1,
+        RIP = 120
+    }
+}
---
+++ Router/RoutingTable.cs
@@ -0,0 +1,85 @@
+﻿using Router.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router
+{
+    class RoutingTable
+    {
+        internal static RoutingTable Instance { get; } = new RoutingTable();
+
+        private List<RoutingEntry> Entries = new List<RoutingEntry>();
+
+        public void Push(RoutingEntry RoutingEntry)
+        {
+            var index = Entries.FindIndex(Entry => Entry.IPNetwork == RoutingEntry.IPNetwork && Entry.ADistance == RoutingEntry.ADistance);
+            if (index != -1)
+            {
+                Entries[index] = RoutingEntry;
+                return;
+            }
+
+            Entries.Add(RoutingEntry);
+        }
+
+        public RoutingEntry Find(IPNetwork IPNetwork, ADistance ADistance)
+        {
+            return Entries.Find(Entry => Entry.IPNetwork == IPNetwork && Entry.ADistance == ADistance);
+        }
+
+        public RoutingEntry Lookup(IPAddress IPAddress)
+        {
+            RoutingEntry BestMatch = null;
+            foreach (var Entry in Entries)
+            {
+                if (!Entry.IPNetwork.Contains(IPAddress))
+                {
+                    continue;
+                }
+
+                if(BestMatch == null || BestMatch.IPNetwork >= Entry.IPNetwork || BestMatch.ADistance < Entry.ADistance)
+                {
+                    if(BestMatch.NextHopIP == null)
+                    {
+                        Entry.NextHopIP = BestMatch.NextHopIP;
+                    }
+
+                    BestMatch = Entry;
+                }
+            }
+
+            return BestMatch;
+        }
+
+        public bool Remove(IPNetwork IPNetwork, ADistance ADistance)
+        {
+            var index = Entries.FindIndex(Entry => Entry.IPNetwork == IPNetwork && Entry.ADistance == ADistance);
+            if(index != -1)
+            {
+                Entries.RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+        
+        public bool Exists(IPNetwork IPNetwork)
+        {
+            return Entries.Exists(Entry => Entry.IPNetwork == IPNetwork);
+        }
+
+        public bool Exists(IPAddress IPAddress)
+        {
+            return Entries.Exists(Entry => Entry.IPNetwork.Contains(IPAddress));
+        }
+
+        public List<RoutingEntry> GetEntries()
+        {
+            return Entries;
+        }
+    }
+}
