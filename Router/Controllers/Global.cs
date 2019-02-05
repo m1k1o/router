a7c48ae53107b59a9ff38db073749aee3d047567
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-05 13:59:43 +0100

    Global API methods.

---
+++ Router/Controllers/Global.cs
@@ -0,0 +1,29 @@
+﻿using Router.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router.Controllers
+{
+    class Global
+    {
+        public JSON Initialize(string Data)
+        {
+            var obj = new JSONObject();
+            obj.Push("interfaces", (new Interfaces()).Show());
+            obj.Push("arp_table", (new ARP()).Table());
+            obj.Push("routing_table", (new Routing()).Table());
+            return obj;
+        }
+
+        public JSON Update(string Data)
+        {
+            var obj = new JSONObject();
+            obj.Push("arp_table", (new ARP()).Table());
+            obj.Push("routing_table", (new Routing()).Table());
+            return obj;
+        }
+    }
+}
