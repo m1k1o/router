c477f694f68584ace4da9dda56ab687888898688
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-08 00:16:03 +0100

    RIP unicast send available.

--- Router/Protocols/RIP.cs
+++ Router/Protocols/RIP.cs
@@ -6,24 +6,29 @@ namespace Router.Protocols
 {
     class RIP
     {
-        public const int PortUDP = 520;
-        public const string MulticastIp = "224.0.0.9";
-        public const string MulticastMac = "01-00-5E-00-00-09";
+        public static ushort PortUDP = 520;
+        public static IPAddress MulticastIp = IPAddress.Parse("224.0.0.9");
+        public static PhysicalAddress MulticastMac = PhysicalAddress.Parse("01-00-5E-00-00-09");
 
-        static public void Send(RIPCommandType CommandType, RIPRouteCollection RTEs, Interface Interface)
+        static public void Send(PhysicalAddress DstMAC, IPAddress DstIP, ushort DstPort, RIPCommandType CommandType, RIPRouteCollection RTEs, Interface Interface)
         {
             var ripPacket = new RIPPacket(CommandType, RTEs);
 
-            var udpPacket = new UdpPacket(PortUDP, PortUDP);
+            var udpPacket = new UdpPacket(PortUDP, DstPort);
             udpPacket.PayloadData = ripPacket.Bytes;
 
-            var ipPacket = new IPv4Packet(Interface.IPAddress, IPAddress.Parse(MulticastIp));
+            var ipPacket = new IPv4Packet(Interface.IPAddress, DstIP);
             ipPacket.PayloadPacket = udpPacket;
 
-            var ethernetPacket = new EthernetPacket(Interface.PhysicalAddress, PhysicalAddress.Parse(MulticastMac), EthernetPacketType.IpV4);
+            var ethernetPacket = new EthernetPacket(Interface.PhysicalAddress, DstMAC, EthernetPacketType.IpV4);
             ethernetPacket.PayloadPacket = ipPacket;
 
             Interface.SendPacket(ethernetPacket.Bytes);
+        }
+
+        static public void Send(RIPCommandType CommandType, RIPRouteCollection RTEs, Interface Interface)
+        {
+            Send(MulticastMac, MulticastIp, PortUDP, CommandType, RTEs, Interface);
         }
 
         static public RIPPacket Parse(EthernetPacket packet, Interface Interface = null)
