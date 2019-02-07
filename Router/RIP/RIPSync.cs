7a808444cee3667db8243d11012890ba147de35c
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-07 21:08:56 +0100

    RIP Sync - class.

---
+++ Router/RIP/RIPSync.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router.RIP
+{
+    class RIPSync
+    {
+        public void Import()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Export()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
