ee0349023e9bb12ffb8c8eed6b26c534f9db5ee9
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-07 23:10:00 +0100

    RIP Update - Export.

---
+++ Router/RIP/RIPUpdate.cs
@@ -0,0 +1,54 @@
+﻿using Router.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router.RIP
+{
+    class RIPUpdate
+    {
+        public static bool SplitHorizon = true;
+        public static bool PoisonReverse = false;
+
+
+        public static void Import(RIPRouteCollection RouteCollection, Interface Interface)
+        {
+            throw new NotFiniteNumberException();
+        }
+
+        public static RIPRouteCollection Export(Interface Interface)
+        {
+            var RouteCollection = new RIPRouteCollection();
+            var Routes = RIPTable.Instance.BestEntries();
+
+            foreach (var Route in Routes)
+            {
+                uint Metric;
+
+                // Split Hotizon \w Poison Reverse
+                if (SplitHorizon && Route.Interface == Interface)
+                {
+                    if (PoisonReverse)
+                    {
+                        Metric = 16;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Metric = Route.Metric;
+                }
+
+                RouteCollection.Add(Route.IPNetwork.NetworkAddress, Route.IPNetwork.SubnetMask, Interface.IPAddress, Metric);
+            }
+
+            return RouteCollection;
+        }
+    }
+}
