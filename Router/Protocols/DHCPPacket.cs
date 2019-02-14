f00a45d4be94fc2785dea22b7f87dc7ceb052108
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-14 20:30:51 +0100

    Revert "bad HardwareType."
    This reverts commit 421ff17593b3388f0cd2e5a27ec514e5edf3308f.

--- Router/Protocols/DHCPPacket.cs
+++ Router/Protocols/DHCPPacket.cs
@@ -17,9 +17,9 @@ namespace Router.Protocols
         }
 
         // Hardware address type.
-        public PacketDotNet.LinkLayers HardwareType
+        public PacketDotNet.ARPOperation HardwareType
         {
-            get => (PacketDotNet.LinkLayers)Slice(1, typeof(byte));
+            get => (PacketDotNet.ARPOperation)Slice(1, typeof(byte));
             set => Inject(1, (byte)value);
         }
 
