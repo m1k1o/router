f33108c49b3a73aa721d29c98a7647c6190dfc47
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-03-04 14:15:44 +0100

    Analyzer idea added.

---
+++ Router/Analyzer/Scenario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router.Analyzer
+{
+    class Scenario
+    {
+        public Interface SourceInterface { get; set; }
+        public Interface TargetInterface { get; set; }
+
+        public List<TestCase> TestCases { get; set; } = new List<TestCase>();
+
+        private TestCase CurrentTestCase = null;
+
+        public void Execute()
+        {
+            SourceInterface.OnPacketArrival += OnPacketArrival;
+
+            // While:
+                // Generate packets.
+
+                // Wait Timeout.
+
+                // Analyze otuputs.
+                
+            SourceInterface.OnPacketArrival -= OnPacketArrival;
+            
+            // Analyze results
+        }
+
+        public void OnPacketArrival(Handler Handler)
+        {
+            //CurrentTestCase.Analyze(Handler);
+            Console.WriteLine("Target received Packet.");
+        }
+    }
+}
---
+++ Router/Analyzer/TestCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router.Analyzer
+{
+    class TestCase
+    {
+        // LOG
+
+        public byte[] Generate()
+        {
+            // Generate test packet.
+            return new byte[0];
+        }
+
+        public bool Analyze(Handler Handler)
+        {
+            // Analyze received packet.
+            return false;
+        }
+    }
+}
