5837df18a1bf74c0bbd0e8c654079891efb6d81a
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-03-05 20:19:12 +0100

    Interface service moved to interface.

--- Router/Interface.cs
+++ Router/Interface.cs
@@ -175,4 +175,25 @@ namespace Router
 
         public override bool Equals(object obj) => !(obj is null) && obj.GetType() == GetType() && Equals(obj as Interface);
     }
+
+    interface InterfaceService
+    {
+        string Name { get; }
+
+        string Description { get; }
+
+        bool OnlyRunningInterface { get; }
+
+        bool DefaultRunning { get; }
+
+        bool Anonymous { get; }
+
+        void OnStarted(Interface Interface);
+
+        void OnStopped(Interface Interface);
+
+        void OnChanged(Interface Interface);
+
+        void OnPacketArrival(Handler Handler);
+    }
 }
