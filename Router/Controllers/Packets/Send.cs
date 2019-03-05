1b1c7e3be12d74a90802a62892c0289426664e20
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-03-06 00:59:47 +0100

    Controllers Packet.

---
+++ Router/Controllers/Packets/Send.cs
@@ -0,0 +1,24 @@
+﻿using Router.Packets;
+using System;
+
+namespace Router.Controllers.Packets
+{
+    class Send : Controller, Executable
+    {
+        public Interface Interface { get; set; }
+
+        public GeneratorPacket Packet { get; set; }
+
+        public void Execute()
+        {
+            if (Interface == null || Packet == null)
+            {
+                throw new Exception("Expected Interface and Packet.");
+            }
+
+            Interface.SendPacket(Packet.Export());
+        }
+
+        public object Export() => this;
+    }
+}
