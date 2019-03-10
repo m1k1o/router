aee6d0b1d6093984eae7cb5e629734b0e269632b
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-03-10 15:21:33 +0100

    Storage Logic extracted to class.

--- Router/Analyzer/TestCaseStorage.cs
+++ Router/Analyzer/TestCaseStorage.cs
@@ -1,111 +1,24 @@
 ﻿using Router.Helpers;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace Router.Analyzer
 {
     static class TestCaseStorage
     {
-        const string FILE_NAME = "TestCases.json";
+        private static Storage<TestCase> Storage = new Storage<TestCase>("TestCases.json");
 
-        private static Dictionary<int, TestCase> Instances = new Dictionary<int, TestCase>();
-        private static int Index = 0;
+        public static int Insert(TestCase TestCase) => Storage.Insert(TestCase);
 
-        static TestCaseStorage()
-        {
-            try
-            {
-                LoadFromFile();
-            }
-            catch { }
-        }
+        internal static void Edit(int Index, TestCase TestCase) => Storage.Edit(Index, TestCase);
 
-        static void SaveToFile()
-        {
-            // Serialize TestCases
-            var JSONString = JSON.SerializeObject(Export());
+        public static void Remove(int Index) => Storage.Remove(Index);
 
-            // Save to File
-            File.WriteAllText(FILE_NAME, JSONString, Encoding.UTF8);
-        }
+        public static TestCase Get(int Index) => Storage.Get(Index);
 
-        static void LoadFromFile()
-        {
-            // Get JSON String
-            var JsonString = File.ReadAllText(FILE_NAME, Encoding.UTF8);
+        public static Dictionary<int, TestCase> Import(List<TestCase> List) => Storage.Import(List);
 
-            // Parse & Import
-            var TestCases = new List<TestCase>();
-            JSON.PopulateObject(JsonString, TestCases);
+        public static List<TestCase> Export() => Storage.Export();
 
-            foreach (var TestCase in TestCases)
-            {
-                Instances.Add(Index++, TestCase);
-            }
-        }
-
-        public static int Insert(TestCase TestCase)
-        {
-            Instances.Add(Index, TestCase);
-
-            try
-            {
-                SaveToFile();
-            }
-            catch { }
-
-            return Index++;
-        }
-
-        internal static void Edit(int Index, TestCase TestCase)
-        {
-            Instances[Index] = TestCase;
-        }
-
-        public static void Remove(int Index)
-        {
-            Instances.Remove(Index);
-
-            try
-            {
-                SaveToFile();
-            }
-            catch { }
-        }
-
-        public static TestCase Get(int Index)
-        {
-            return Instances[Index];
-        }
-
-        public static Dictionary<int, TestCase> Import(List<TestCase> List)
-        {
-            var NewEntries = new Dictionary<int, TestCase>();
-            foreach (var TestCase in List)
-            {
-                NewEntries.Add(Index, TestCase);
-                Instances.Add(Index++, TestCase);
-            }
-
-            try {
-                SaveToFile();
-            }
-            catch { }
-
-            return NewEntries;
-        }
-
-        public static List<TestCase> Export()
-        {
-            return Instances.Values.ToList();
-        }
-
-        public static Dictionary<int, TestCase> GetAll()
-        {
-            return Instances;
-        }
+        public static Dictionary<int, TestCase> GetAll() => Storage.GetAll();
     }
 }
---
+++ Router/Helpers/Storage.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Router.Helpers
+{
+    class Storage<TValue>
+    {
+        private Dictionary<int, TValue> Instances = new Dictionary<int, TValue>();
+        private int Index = 0;
+
+        private string FileName = null;
+
+        public Storage(string FileName = null)
+        {
+            this.FileName = FileName;
+            Load();
+        }
+
+        protected void Save()
+        {
+            if (FileName == null)
+            {
+                return;
+            }
+
+            try
+            {
+                // Serialize TestCases
+                var JSONString = JSON.SerializeObject(Export());
+
+                // Save to File
+                File.WriteAllText(FileName, JSONString, Encoding.UTF8);
+            }
+            catch { }
+        }
+
+        protected void Load()
+        {
+            if (FileName == null)
+            {
+                return;
+            }
+
+            try
+            {
+                // Get JSON String
+                var JsonString = File.ReadAllText(FileName, Encoding.UTF8);
+
+                // Parse
+                var TestCases = new List<TValue>();
+                JSON.PopulateObject(JsonString, TestCases);
+
+                // Import
+                foreach (var TestCase in TestCases)
+                {
+                    Instances.Add(Index++, TestCase);
+                }
+            }
+            catch { }
+        }
+
+        public int Insert(TValue Value)
+        {
+            Instances.Add(Index, Value);
+            Save();
+            return Index++;
+        }
+
+        public void Edit(int Index, TValue Value)
+        {
+            Instances[Index] = Value;
+            Save();
+        }
+
+        public void Remove(int Index)
+        {
+            Instances.Remove(Index);
+            Save();
+        }
+
+        public TValue Get(int Index)
+        {
+            return Instances[Index];
+        }
+
+        public Dictionary<int, TValue> Import(List<TValue> List)
+        {
+            var NewEntries = new Dictionary<int, TValue>();
+            foreach (var Value in List)
+            {
+                NewEntries.Add(Index, Value);
+                Instances.Add(Index++, Value);
+            }
+
+            Save();
+            return NewEntries;
+        }
+
+        public List<TValue> Export()
+        {
+            return Instances.Values.ToList();
+        }
+
+        public Dictionary<int, TValue> GetAll()
+        {
+            return Instances;
+        }
+    }
+}
