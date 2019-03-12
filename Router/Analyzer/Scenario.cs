aa9987e84ae6919c02880f9f7ea8fe7892375d43
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-03-12 02:01:43 +0100

    Scenario drafted.

--- Router/Analyzer/Scenario.cs
+++ Router/Analyzer/Scenario.cs
@@ -1,17 +1,21 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Router.Analyzer
 {
     class Scenario : List<TestCase>
     {
+        public event Action OnStarted = delegate { };
+        public event Action OnStopped = delegate { };
+        public event Action<string> OnLogMessage = delegate { };
+
         [JsonIgnore]
         public Interface GeneratorInterface { get; set; }
         [JsonIgnore]
         public Interface AnalyzerInterface { get; set; }
-        
-        /*
+
         public int Total => Count;
         public int Successful { get; private set; }
         public int Percentage => Successful / Total * 100;
@@ -26,7 +30,8 @@ namespace Router.Analyzer
                 if (Running)
                 {
                     return DateTime.Now - TimeStarted;
-                } else
+                }
+                else
                 {
                     return TimeStopped - TimeStarted;
                 }
@@ -34,22 +39,57 @@ namespace Router.Analyzer
         }
 
         public bool Running => TimeStopped == DateTime.MinValue;
-        public string Log { get; private set; }
 
-        public void Execute(Interface GeneratorInterface, Interface AnalyzerInterface)
+        private ManualResetEvent BlocingWaiting = new ManualResetEvent(false);
+        private Thread Thread;
+
+        public void RunTestCase(TestCase TestCase)
+        {
+            TestCase.GeneratorInterface = GeneratorInterface;
+            TestCase.AnalyzerInterface = AnalyzerInterface;
+
+            // Register events
+            Action OnStopped = null;
+            TestCase.OnLogMessage += OnLogMessage;
+            TestCase.OnStopped += OnStopped = () =>
+            {
+                if (TestCase.Status == TestCaseStatus.Success)
+                {
+                    Successful++;
+                }
+
+                // Unregister events
+                TestCase.OnLogMessage -= OnLogMessage;
+                TestCase.OnStopped -= OnStopped;
+                BlocingWaiting.Set();
+            };
+
+            // Start test case
+            BlocingWaiting.Reset();
+            TestCase.Start();
+        }
+
+        public void Start()
         {
             TimeStarted = DateTime.Now;
             TimeStopped = DateTime.MinValue;
             Successful = 0;
-            foreach (var TestCase in this)
-            {
-                TestCase.Execute(GeneratorInterface, AnalyzerInterface);
-                Log += TestCase.Log;
+            OnStarted();
 
-                if (TestCase.Success) Successful++;
-            }
-            TimeStopped = DateTime.Now;
+            // Wait for result
+            Thread = new Thread(() => {
+                foreach (var TestCase in this)
+                {
+                    RunTestCase(TestCase);
+
+                    // Wait for complete
+                    BlocingWaiting.WaitOne();
+                }
+
+                TimeStopped = DateTime.Now;
+                OnStopped();
+            });
+            Thread.Start();
         }
-        */
      }
 }
