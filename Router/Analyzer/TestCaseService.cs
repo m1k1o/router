c4139b9acad7420d2d7f3768afe1e3f5dcfbba69
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-03-12 01:40:14 +0100

    TestCase WS: Status as integer.

--- Router/Analyzer/TestCaseService.cs
+++ Router/Analyzer/TestCaseService.cs
@@ -16,7 +16,7 @@ namespace Router.Analyzer
                 HTTP.WebSockets.Send(Client, "test_case", new
                 {
                     Running = true,
-                    Status = TestCase.Status.ToString(),
+                    TestCase.Status,
                     TimeOut = TestCase.Timeout.TotalSeconds
                 });
             };
@@ -25,7 +25,7 @@ namespace Router.Analyzer
                 HTTP.WebSockets.Send(Client, "test_case", new
                 {
                     Running = false,
-                    Status = TestCase.Status.ToString()
+                    TestCase.Status
                 });
                 Stop(Client);
             };
